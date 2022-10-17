893ec437012a555c1b8138d6a690931cb6ed76a9
caltistals <y.db.qp.y@gmail.com>
2022-10-18 04:03:52 +0900

    [update] useFormを導入

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,38 +1,61 @@
-import React from "react";
-import { useForm } from "@mantine/form";
 import {
   TextInput,
-  Center,
-  Group,
   Paper,
   Container,
   Title,
   PasswordInput,
   Button,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
 const AuthForm = () => {
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validate: {
+      email: (value) =>
+        /^\S+@\S+$/.test(value)
+          ? null
+          : "正しいメールアドレスを入力してください",
+      password: (value) =>
+        value.length < 6 ? "6文字以上のパスワードを入力してください" : null,
+      confirmPassword: (value, values) =>
+        value !== values.password ? "パスワードが一致していません" : null,
+    },
+  });
+
   return (
     <Container size={400} my={40}>
       <Title align="center" color="cyan.9">
-        Welcome back
+        新規登録
       </Title>
-      <Paper p="xl" mx="auto" my="md" withBorder>
-        <form>
+      <Paper p="xl" mx="auto" my="lg" withBorder>
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
           <TextInput
             withAsterisk
             label="メールアドレス"
             placeholder="xxx@example.com"
-            required
+            {...form.getInputProps("email")}
           />
           <PasswordInput
             withAsterisk
             label="パスワード"
             placeholder="●●●●●●"
             mt="md"
+            {...form.getInputProps("password")}
+          />
+          <PasswordInput
+            withAsterisk
+            label="パスワードの確認"
+            placeholder="●●●●●●"
+            mt="md"
+            {...form.getInputProps("confirmPassword")}
           />
           <Button type="submit" color="cyan.6" mt="xl" fullWidth>
-            ログイン
+            新規登録
           </Button>
         </form>
       </Paper>
