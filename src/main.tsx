aecdc0776e1e8ad3c24f0811e4629eb4a679ce55
caltistals <y.db.qp.y@gmail.com>
2022-10-28 23:48:29 +0900

    init firebase

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase-config";
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
