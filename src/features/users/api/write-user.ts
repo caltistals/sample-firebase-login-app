3fb41c69ccaa44d1245b771ff10033f5d293e4a3
caltistals <y.db.qp.y@gmail.com>
2022-11-03 21:01:28 +0900

    [add] ユーザー情報書き込み用の関数を追加

---
+++ src/features/users/api/write-user.ts
@@ -0,0 +1,24 @@
+import { collection, Firestore, getDoc, doc, setDoc } from "firebase/firestore";
+import { UserType } from "../types";
+
+export const writeUser = async (db: Firestore, newUser: UserType) => {
+  const id = newUser.id;
+
+  let theUser: UserType | null = null;
+  const userDoc = await getDoc(doc(collection(db, "users"), id));
+  if (!userDoc.exists()) {
+    const user: UserType = {
+      id: id,
+    };
+    await setDoc(userDoc.ref, {
+      ...user,
+    });
+    theUser = { ...user };
+  } else {
+    await setDoc(userDoc.ref, {
+      ...newUser,
+    });
+    theUser = { ...newUser };
+  }
+  return writeUser;
+};
