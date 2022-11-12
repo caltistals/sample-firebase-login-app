649e6f372a8ff642b14e9394b05efcdf63e54e62
caltistals <y.db.qp.y@gmail.com>
2022-11-12 22:24:42 +0900

    [add] グループ作成関数を定義

---
+++ src/features/group/api/create-group.ts
@@ -0,0 +1,21 @@
+import { collection, Firestore, getDoc, doc, setDoc } from "firebase/firestore";
+import { writeUser } from "../../users/api/write-user";
+import { UserType } from "../../users/types";
+import { GroupType } from "../types/index";
+
+export const createGroup = async (
+  db: Firestore,
+  user: UserType,
+  groupName: string
+) => {
+  const id = user.id;
+  const newGroupRef = doc(collection(db, "group"));
+  const newGroup: GroupType = {
+    groupId: newGroupRef.id,
+    groupName: groupName,
+    users: [user],
+    creator: user,
+  };
+  await setDoc(newGroupRef, newGroup);
+  await writeUser(db, { ...user, groupId: newGroupRef.id });
+};
