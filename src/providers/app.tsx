f15b076de0ff4acaa303b753a7de22e59317737c
caltistals <y.db.qp.y@gmail.com>
2022-12-23 23:58:10 +0900

    add: Mantine Notification

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -2,13 +2,16 @@ import { FC, PropsWithChildren } from "react";
 import { MantineProvider } from "@mantine/core";
 import { BrowserRouter as Router } from "react-router-dom";
 import FirebaseApp from "./FirebaseApp";
+import { NotificationsProvider } from "@mantine/notifications";
 
 export const AppProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <FirebaseApp>
-        <Router>{children}</Router>
-      </FirebaseApp>
+      <NotificationsProvider>
+        <FirebaseApp>
+          <Router>{children}</Router>
+        </FirebaseApp>
+      </NotificationsProvider>
     </MantineProvider>
   );
 };
