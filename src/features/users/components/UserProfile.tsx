8b04ee248f0e02cc994ffd9f0b545a35da9a3976
caltistals <y.db.qp.y@gmail.com>
2023-01-09 06:06:30 +0900

    update: UserProfileのUIを調整
    全体をCardでラップするようにしました。
    タイトルの文字色を変更しました。

--- src/features/users/components/UserProfile.tsx
+++ src/features/users/components/UserProfile.tsx
@@ -15,39 +15,41 @@ const UserProfile = () => {
   const { user, handleLogout } = useLogout();
 
   return (
-    <Container size={400} my={40}>
-      <Title order={2} align="center" color="cyan.5" mb="lg">
-        ユーザー情報
-      </Title>
-      <Stack>
-        {user && user.avatarColor && user.displayName ? (
-          <>
-            <Card>
-              <Stack>
-                <Group position="center">
-                  <Avatar
-                    color={user.avatarColor}
-                    radius="xl"
-                    variant="filled"
-                  />
-                </Group>
-                <Group position="center">
-                  <Text size="xl">{user.displayName}</Text>
-                </Group>
-              </Stack>
-            </Card>
-          </>
-        ) : null}
-        <Button
-          type="submit"
-          color="red"
-          mt="xl"
-          variant="outline"
-          onClick={handleLogout}
-        >
-          ログアウト
-        </Button>
-      </Stack>
+    <Container size={500}>
+      <Card withBorder shadow="sm" radius="sm">
+        <Title order={2} align="center" color="dark.3" mb="lg">
+          ユーザー情報
+        </Title>
+        <Stack>
+          {user && user.avatarColor && user.displayName ? (
+            <>
+              <Card>
+                <Stack>
+                  <Group position="center">
+                    <Avatar
+                      color={user.avatarColor}
+                      radius="xl"
+                      variant="filled"
+                    />
+                  </Group>
+                  <Group position="center">
+                    <Text size="xl">{user.displayName}</Text>
+                  </Group>
+                </Stack>
+              </Card>
+            </>
+          ) : null}
+          <Button
+            type="submit"
+            color="red"
+            mt="xl"
+            variant="outline"
+            onClick={handleLogout}
+          >
+            ログアウト
+          </Button>
+        </Stack>
+      </Card>
     </Container>
   );
 };
