5dafb65c9f8af2098c7e52d03acd25144bdbd21a
caltistals <y.db.qp.y@gmail.com>
2023-01-09 06:02:11 +0900

    update: DinnerPlansのUIを調整
    全体をCardでラップするようにしました。
    日付とタイトルをCardのSectionに移動しました。
    dinnerPlansがnullのときに表示されるメッセージを変更しました。
    読み込み中は上記メッセージを表示させないように変更しました。

--- src/features/dinner/components/DinnerPlans.tsx
+++ src/features/dinner/components/DinnerPlans.tsx
@@ -1,46 +1,78 @@
-import { Accordion, Center, Loader, Text } from "@mantine/core";
+import {
+  Accordion,
+  ActionIcon,
+  Card,
+  Center,
+  Container,
+  Group,
+  Loader,
+  Text,
+  ThemeIcon,
+  Title,
+} from "@mantine/core";
 import { DinnerPlan } from "./DinnerPlan";
 import dayjs from "dayjs";
 import useReadDinnerPlans from "../hooks/useReadDinnerPlans";
+import { IconAlertCircle } from "@tabler/icons";
 
 export const DinnerPlans = () => {
   const { dinnerPlans, isLoading } = useReadDinnerPlans();
   const date = new Date();
   return (
     <>
-      <Text size="xl" mb="md" weight={700}>
-        {dayjs(date).format("YYYY年MM月DD日")}の予定
-      </Text>
-      {isLoading && (
-        <Center>
-          <Loader />
-        </Center>
-      )}
-      {!isLoading && dinnerPlans ? (
-        <div>
-          <Accordion
-            styles={{
-              item: {
-                // styles added to all items
-                backgroundColor: "#fff",
+      <Container fluid>
+        <Card withBorder shadow="sm" radius="sm">
+          <Card.Section withBorder inheritPadding py="xs" mb="xs">
+            <Title order={3} color="dark.3" weight={700}>
+              晩御飯の予定
+            </Title>
+            <Text color="dark.2">{dayjs(date).format("YYYY年MM月DD日")}</Text>
+          </Card.Section>
 
-                // // styles added to expanded item
-                // "&[data-active]": {
-                //   backgroundColor: "#ffd803",
-                // },
-              },
-            }}
-            chevronPosition="right"
-            variant="contained"
-          >
-            {dinnerPlans.map((dinnerPlan) => (
-              <DinnerPlan dinnerPlan={dinnerPlan} key={dinnerPlan.user.id} />
-            ))}
-          </Accordion>
-        </div>
-      ) : (
-        <Text>登録されている予定はありません</Text>
-      )}
+          {isLoading && (
+            <Center>
+              <Loader />
+            </Center>
+          )}
+          {!isLoading && dinnerPlans ? (
+            <div>
+              <Accordion
+                styles={{
+                  item: {
+                    // styles added to all items
+                    backgroundColor: "#fff",
+
+                    // // styles added to expanded item
+                    // "&[data-active]": {
+                    //   backgroundColor: "#ffd803",
+                    // },
+                  },
+                }}
+                chevronPosition="right"
+                variant="contained"
+              >
+                {dinnerPlans.map((dinnerPlan) => (
+                  <DinnerPlan
+                    dinnerPlan={dinnerPlan}
+                    key={dinnerPlan.user.id}
+                  />
+                ))}
+              </Accordion>
+            </div>
+          ) : (
+            <>
+              {!isLoading && (
+                <Group>
+                  <ActionIcon variant="transparent">
+                    <IconAlertCircle size={20} />
+                  </ActionIcon>
+                  <Text color="dark.2">予定が登録されていません</Text>
+                </Group>
+              )}
+            </>
+          )}
+        </Card>
+      </Container>
     </>
   );
 };
